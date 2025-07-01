a90e23d730cdb710202ced0dffcb1f9e89256864
samuel <samuelgomes101112@gmail.com>
2025-07-01 11:06:29 -0300

    updated file footer end remove poa on index.php

--- footer.php
+++ footer.php
@@ -1,79 +1,94 @@
 <section class="footer-landing" id="contato">
-            <div class="container">
-                <div class="row">
-                    
-                    <div class="col-lg-4 col-12">
+    <div class="container">
+        <div class="row">
 
-                    <div class="col-lg-8  col-12"><img src="https://www.liceubrasil.com.br/sites/liceu/images/logo-dark.png" alt="" class="img-fluid" style="margin-bottom: 50px"></div>
-                        
-                        <h5 style="margin-bottom: -5px;">Fale conosco</h5>
-                        <a href="https://api.whatsapp.com/send?phone=5511953255890" target="_blank" class="btn btn-primary btn-lg " style="padding: 5px 10px; font-size: 18px; border-radius: 30px; background-color: #ffffff; margin: 20px 0; color: #002bbb; font-weight: bold"><i class="mdi mdi-whatsapp"></i> MANDE UMA MENSAGEM</a>
+            <div class="col-lg-4 col-12">
 
-                        <ul class="list-unstyled company-sub-menu">
-                            <li class="mb-3"><a class="link-footer-landing" href="#"><i class="mdi mdi-phone-in-talk icone-footer-landing"></i> +11 4642-0648</a></li>
-                            <li class="mb-3"><a class="link-footer-landing" href="#"><i class="mdi mdi-whatsapp icone-footer-landing"></i> (11) 95325-5890</a></li>
-                            <li class="mb-3"><a class="link-footer-landing" href="#"><i class="mdi mdi-email icone-footer-landing"></i> contato@liceubrasil.com.br</a></li>
-                        </ul>
-                    </div>
-                    <!--end col-->
-                    <div class="col-lg-4 col-12" style="margin-bottom: 30px">
-                        <h4 class="mb-3">Faça uma visita</h4>                        
+                <div class="col-lg-8  col-12"><img src="https://www.liceubrasil.com.br/sites/liceu/images/logo-dark.png" alt="" class="img-fluid" style="margin-bottom: 50px"></div>
 
-                        <h6 style="margin-top: 40px"><b>Itaquaquecetuba</b></h6>
-                        <div class="col-lg-12" style="margin-top: 10px">
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
-                                <div class="col-lg-11 col-11">R. da Liberdade, 45 <br> Centro, Itaquaquecetuba - SP</div>
-                            </div>
-                        </div>
+                <h5 style="margin-bottom: -5px;">Fale conosco</h5>
+                <a href="https://api.whatsapp.com/send?phone=5511953255890" target="_blank" class="btn btn-primary btn-lg " style="padding: 5px 10px; font-size: 18px; border-radius: 30px; background-color: #ffffff; margin: 20px 0; color: #002bbb; font-weight: bold"><i class="mdi mdi-whatsapp"></i> MANDE UMA MENSAGEM</a>
 
-                        <h6 style="margin-top: 10px"><b>Suzano</b></h6>
-                        <div class="col-lg-12" style="margin-top: 10px">
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
-                                <div class="col-lg-11 col-11">R. Tiradentes, 235 <br> Centro, Suzano - SP</div>
-                            </div>
-                        </div>
-
-                         <h6 style="margin-top: 10px"><b>Aruja</b></h6>
-                        <div class="col-lg-12" style="margin-top: 10px">
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
-                                <div class="col-lg-11 col-11">R. Raposo Taváres, 176<br> Jardim Renata, Arujá - SP</div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <!--end col-->
-                    <div class="col-lg-4  col-12">
-                        <h4 class="mb-3">Acompanhe as novidades</h4>
-                        
-                        <div class="col-lg-12" style="margin-top: 40px">
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
-                                <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceubrasil/" target="_blank">@liceubrasil</a></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
-                                <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceudabeleza?igsh=MXV4bTVmZWwzYTNiMQ==" target="_blank">@belezaitaqua</a></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
-                                <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceudabelezasuzano?igsh=dGd6MXNmYm45b3ky" target="_blank">@belezasuzano</a></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
-                                <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceudabeleza.aruja?igsh=cm9uNjN2bHVsOGVw" target="_blank">@belezaaruja</a></div>
-                            </div>               
-                        </div>
-                        
-                    </div>
-                    <!--end col-->
-                </div>
-                <!--end row-->
+                <ul class="list-unstyled company-sub-menu">
+                    <li class="mb-3"><a class="link-footer-landing" href="#"><i class="mdi mdi-phone-in-talk icone-footer-landing"></i> +11 4642-0648</a></li>
+                    <li class="mb-3"><a class="link-footer-landing" href="https://api.whatsapp.com/send?phone=5511953255890"><i class="mdi mdi-whatsapp icone-footer-landing"></i> (11) 95325-5890</a></li>
+                    <li class="mb-3"><a class="link-footer-landing" href="#"><i class="mdi mdi-email icone-footer-landing"></i> contato@liceubrasil.com.br</a></li>
+                </ul>
             </div>
-            <!--end container-->
-        </section>
-        
-        <!--end footer-alt-->
-        <!-- END FOOTER -->
+            <!--end col-->
+            <div class="col-lg-4 col-12" style="margin-bottom: 30px">
+                <h4 class="mb-3">Faça uma visita</h4>
+
+                <h6 style="margin-top: 40px"><b>Itaquaquecetuba</b></h6>
+                <div class="col-lg-12" style="margin-top: 10px">
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
+                        <a href="https://www.google.com/maps/place/Liceu+Brasil/@-23.4740826,-46.3502862,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce7cbbcc4c02fb:0x2a88b11fbbccfaa7!8m2!3d-23.4740826!4d-46.3502862!16s%2Fg%2F11bvt5hclc?entry=ttu&g_ep=EgoyMDI1MDYyNi4wIKXMDSoASAFQAw%3D%3D">
+                            <div class="col-lg-11 col-11">R. da Liberdade, 45 <br> Centro, Itaquaquecetuba - SP</div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-12" style="margin-top: 10px">
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
+                        <a href="https://www.google.com/maps/place/Liceu+da+Beleza/data=!4m2!3m1!1s0x0:0x4a51ec0bee9603ab?sa=X&ved=1t:2428&ictx=111">
+                            <div class="col-lg-11 col-11">R. da Liberdade, 104 <br> Centro, Itaquaquecetuba - SP</div>
+                        </a>
+                    </div>
+                </div>
+
+                <h6 style="margin-top: 10px"><b>Suzano</b></h6>
+                <div class="col-lg-12" style="margin-top: 10px">
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
+                        <a href="https://www.google.com/maps/place/Liceu+da+Beleza+Suzano/@-23.5360242,-46.310855,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce7bceaf7cd2dd:0xd637afa5a9f3f566!8m2!3d-23.5360242!4d-46.310855!16s%2Fg%2F11vt06wwv8?entry=ttu&g_ep=EgoyMDI1MDYyNi4wIKXMDSoASAFQAw%3D%3D">
+                            <div class="col-lg-11 col-11">R. Felício de Camargo, 216 <br> Centro, Suzano - SP</div>
+                        </a>
+                    </div>
+                </div>
+
+                <h6 style="margin-top: 10px"><b>Aruja</b></h6>
+                <div class="col-lg-12" style="margin-top: 10px">
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 36px; padding: 0"><i class="mdi mdi-map-marker"></i></div>
+                        <a href="https://www.google.com/maps/place/Liceu+da+Beleza+Aruja/@-23.3956001,-46.3199515,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce87cbd16de797:0x72576b69590226a1!8m2!3d-23.3956001!4d-46.3199515!16s%2Fg%2F11y74zcl83?entry=ttu&g_ep=EgoyMDI1MDYyNi4wIKXMDSoASAFQAw%3D%3D">
+                            <div class="col-lg-11 col-11">R. Raposo Taváres, 176<br> Jardim Renata, Arujá - SP</div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <!--end col-->
+            <div class="col-lg-4  col-12">
+                <h4 class="mb-3">Acompanhe as novidades</h4>
+
+                <div class="col-lg-12" style="margin-top: 40px">
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
+                        <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceubrasil/" target="_blank">@liceubrasil</a></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
+                        <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceudabeleza?igsh=MXV4bTVmZWwzYTNiMQ==" target="_blank">@belezaitaqua</a></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
+                        <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceudabelezasuzano?igsh=dGd6MXNmYm45b3ky" target="_blank">@belezasuzano</a></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-1 col-1" style="font-size: 21px; padding: 0"><i class="mdi mdi-instagram"></i></div>
+                        <div class="col-lg-11 col-11" style="font-size: 18px; padding: 0 0 0 5px;"><a class="link-footer-landing" href="https://www.instagram.com/liceudabeleza.aruja?igsh=cm9uNjN2bHVsOGVw" target="_blank">@belezaaruja</a></div>
+                    </div>
+                </div>
+
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+    </div>
+    <!--end container-->
+</section>
+
+<!--end footer-alt-->
+<!-- END FOOTER -->
--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 <!-- STRAT NAVBAR -->
 <nav id="navbar" class="navbar navbar-expand-lg fixed-top sticky">
     <a class="navbar-brand" href="./" style="padding: 0 50px">
-        <img src="assets/images/logo-dark.png" alt="logo" height="60" class="logo-light" />
-        <img src="assets/images/logo-dark.png" alt="logo" height="60" class="logo-dark" />
+        <img src="assets/images/logo-dark.png" alt="logo" height="50" class="logo-light" />
+        <img src="assets/images/logo-dark.png" alt="logo" height="40" class="logo-dark" />
     </a>
     <div class="container">
-
+        <!-- ## Botao barra nav ##
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
             aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation" style="padding: 0 50px">
             <span class="mdi mdi-menu" style="color: #FFFFFF !important"></span>
-        </button>
+        </button> -->
         <!--end button-->
 
         <!--end collapse-->
